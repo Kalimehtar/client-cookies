d0033ee51aa461bdc81c9e28e1f9b7c631310d2e
Kalimehtar <kalimehtar@mail.ru>
2014-01-12 15:47:08 +0600

    First release (beta)

--- client-cookies/main.rkt
+++ client-cookies/main.rkt
@@ -2,13 +2,13 @@
 (require "dev.rkt" net/url)
 (provide save-cookies! headers+cookies get-pure-port/cookies get-pure-port/headers/cookies)
 
-(define (get-pure-port/headers/cookies url headers #:redirections [redirections 0] #:status? [status #f])
+(define (get-pure-port/headers/cookies url [headers '()] #:redirections [redirections 0] #:status? [status #f])
   (define-values (port reply-headers) 
-    (get-pure-port/headers url (headers+cookies url headers) #:redirections redirections #:status status))
-  (save-cookies! reply-headers)
+    (get-pure-port/headers url (headers+cookies url headers) #:redirections redirections #:status? status))
+  (save-cookies! url reply-headers)
   (values port reply-headers))
 
-(define (get-pure-port/cookies url headers #:redirections [redirections 0])
+(define (get-pure-port/cookies url [headers '()] #:redirections [redirections 0])
   (call-with-values (get-pure-port/headers/cookies url headers #:redirections redirections)
                     (λ (port _) port)))
     
--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang setup/infotab
-(define version "0.01")
+(define version "0.8")
 (define collection 'multi)
 (define deps '())
